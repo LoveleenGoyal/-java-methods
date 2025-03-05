11b90f332d474bd708592b4b4e239397142ba601
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-05 14:34:21 +0530

    [Loveleen] ADD: Performing actions to check number

---
+++ NumberChecker4.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NumberChecker4 {
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Method to check if a number is a neon number
+    public static boolean isNeon(int number) {
+        int square = number * number;
+        int sum = 0;
+        while (square > 0) {
+            sum += square % 10;
+            square /= 10;
+        }
+        return sum == number;
+    }
+
+    // Method to check if a number is a spy number
+    public static boolean isSpy(int number) {
+        int sum = 0, product = 1;
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            product *= digit;
+            number /= 10;
+        }
+        return sum == product;
+    }
+
+    // Method to check if a number is an automorphic number
+    public static boolean isAutomorphic(int number) {
+        int square = number * number;
+        return String.valueOf(square).endsWith(String.valueOf(number));
+    }
+
+    // Method to check if a number is a buzz number
+    public static boolean isBuzz(int number) {
+        return number % 7 == 0 || number % 10 == 7;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        
+        System.out.println("Number: " + number);
+        System.out.println("Is Prime: " + isPrime(number));
+        System.out.println("Is Neon: " + isNeon(number));
+        System.out.println("Is Spy: " + isSpy(number));
+        System.out.println("Is Automorphic: " + isAutomorphic(number));
+        System.out.println("Is Buzz: " + isBuzz(number));
+    }
+}
